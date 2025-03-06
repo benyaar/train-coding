dfa26d5dd905644e2703b22710cc45991f790e05
Artur <82472015+benyaar@users.noreply.github.com>
2025-03-06 12:06:31 +0200

    task: expanded form

---
+++ index.js
@@ -0,0 +1,43 @@
+import { assert } from 'chai';
+
+
+/* Write Number in Expanded Form
+    You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+   12 --> "10 + 2"
+   45 --> "40 + 5"
+   70304 --> "70000 + 300 + 4"
+*/ 
+//simple solution
+function expandedForm(num) {
+    const b = num.toString().split('')
+    let l = b.length;
+    const f = b.map(e => {
+        for (let i = l - 1; i > 0; i--) {
+            if (e == 0) {
+                continue;
+            }
+            e +='0';
+        }
+        l -=1;
+        return e;
+    })
+    return f.filter(e => e != 0).join(' + ');
+}
+
+//best solution
+const expandedForm2 = n=> n.toString().split('').reverse().map((e,i)=> e * Math.pow(10, i)).filter(e=> e > 0).reverse().join(' + ');
+
+//tests
+describe(" Write Number in Expanded Form", () => {
+  it("func1", () => {
+    assert.strictEqual(expandedForm(12), '10 + 2');
+    assert.strictEqual(expandedForm(42), '40 + 2');
+    assert.strictEqual(expandedForm(70304), '70000 + 300 + 4');
+  });
+  it("func2", () => {
+    assert.strictEqual(expandedForm2(12), '10 + 2');
+    assert.strictEqual(expandedForm2(42), '40 + 2');
+    assert.strictEqual(expandedForm2(70304), '70000 + 300 + 4');
+  });
+});

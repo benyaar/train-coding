a0b3eaab9496c736684050a5bb8d979d07a392b4
Artur <ghostmatt36@gmail.com>
2025-03-09 22:47:11 +0200

    Roman Numerals Encoder

--- index.js
+++ index.js
@@ -300,3 +300,53 @@ describe("Which are in?", () => {
         assert.sameOrderedMembers(inArray2(a1, a2), [])
       });
   });
+  /**
+   * Roman Numerals Encoder
+   *
+  */
+  
+  // Simple solution
+  function solution(n) {
+    const dictionary = {
+        1: 'I', 4: 'IV', 5: 'V', 9: 'IX',
+        10: 'X', 40: 'XL', 50: 'L', 90: 'XC',
+        100: 'C', 400: 'CD', 500: 'D', 900: 'CM',
+        1000: 'M'
+    };
+
+    let result = '';
+    let keys = Object.keys(dictionary).map(Number).reverse();
+
+    for (let key of keys) {
+        while (n >= key) {
+            result += dictionary[key];
+            n -= key;
+        }
+    }
+    return result;
+}
+
+  // Tests
+  describe("Roman Numerals Encoder", function(){
+    it ("should handle small numbers", function(){
+      assert.strictEqual(solution(1), 'I', '1 should return "I"')
+      assert.strictEqual(solution(2), 'II', '2 should return "II"')
+      assert.strictEqual(solution(3), 'III', '3 should return "III"')
+      assert.strictEqual(solution(4), 'IV', '4 should return "IV"')
+      assert.strictEqual(solution(5), 'V', '5 should return "V"')
+      assert.strictEqual(solution(9), 'IX', '9 should return "IX"')
+      assert.strictEqual(solution(10), 'X', '10 should return "X"')
+      assert.strictEqual(solution(11), 'XI', '11 should return "XI"')
+      assert.strictEqual(solution(19), 'XIX', '19 should return "XIX"')
+      assert.strictEqual(solution(22), 'XXII', '22 should return "XXII"')
+      assert.strictEqual(solution(15), 'XV', '15 should return "XV"')
+    });
+    
+    it ("should handle large numbers", function(){
+      assert.strictEqual(solution(1000), 'M', '1000 should, "M"')
+      assert.strictEqual(solution(1001), 'MI', '1001 should, "MI"')
+      assert.strictEqual(solution(1990), 'MCMXC', '1990 should, "MCMXC"')
+      assert.strictEqual(solution(2007), 'MMVII', '2007 should, "MMVII"')
+      assert.strictEqual(solution(2008), 'MMVIII', '2008 should, "MMVIII"')
+    });
+  });

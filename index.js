4e88b0b988d4db8906ba910f6833af2913880866
Artur <ghostmatt36@gmail.com>
2025-03-14 23:16:16 +0200

    Human Readable Time

--- index.js
+++ index.js
@@ -559,3 +559,36 @@ describe("Simple Pig Latin,", () => {
         assert.strictEqual(pigIt('This is my string'), 'hisTay siay ymay tringsay')
     });
 });
+
+/**
+ * Human Readable Time
+ * Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+ * HH = hours, padded to 2 digits, range: 00 - 99
+ * MM = minutes, padded to 2 digits, range: 00 - 59
+ * SS = seconds, padded to 2 digits, range: 00 - 59
+ *
+*/
+
+// Simple solution
+function humanReadable(seconds) {
+    let HH = Math.floor(seconds / 3600);
+    let MM = Math.floor((seconds % 3600) / 60);
+    let SS = seconds % 60;
+
+    return `${HH.toString().padStart(2, 0)}:${MM.toString().padStart(2, 0)}:${SS.toString().padStart(2, 0)}`
+}
+
+// Tests
+describe("Human Readable Time", () => {
+    it("Human Readable Time", () => {
+        assert.strictEqual(humanReadable(59), '00:00:59');
+        assert.strictEqual(humanReadable(60), '00:01:00');
+        assert.strictEqual(humanReadable(90), '00:01:30');
+        assert.strictEqual(humanReadable(3599), '00:59:59');
+        assert.strictEqual(humanReadable(3600), '01:00:00');
+        assert.strictEqual(humanReadable(45296), '12:34:56');
+        assert.strictEqual(humanReadable(86399), '23:59:59');
+        assert.strictEqual(humanReadable(86400), '24:00:00');
+        assert.strictEqual(humanReadable(359999), '99:59:59');
+    });
+});

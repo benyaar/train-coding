23a6ee3147609467759fd55c28099b69f7c6bc7b
Artur <ghostmatt36@gmail.com>
2025-03-23 12:27:06 +0200

    PaginationHelper

--- index.js
+++ index.js
@@ -898,7 +898,7 @@ function score(dice) {
         count
         if (count[key] >= 3) {
             res += dictionary[key]
-            count[key] -=3
+            count[key] -= 3
         }
     }
     if (count[1]) res += count[1] * 100;
@@ -908,27 +908,145 @@ function score(dice) {
 }
 
 // Best solution
-function score2( dice ) {
-    var dc = [0,0,0,0,0,0];
-    var tdr = [1000,200,300,400,500,600];
-    var sdr = [100,0,0,0,50,0];
-    dice.forEach(function(x){ dc[x-1]++; });
-    return dc.reduce(function(s,x,i){ 
-      return s + (x >= 3? tdr[i] : 0) + sdr[i]*(x % 3);
-    },0);
-  }
+function score2(dice) {
+    var dc = [0, 0, 0, 0, 0, 0];
+    var tdr = [1000, 200, 300, 400, 500, 600];
+    var sdr = [100, 0, 0, 0, 50, 0];
+    dice.forEach(function (x) { dc[x - 1]++; });
+    return dc.reduce(function (s, x, i) {
+        return s + (x >= 3 ? tdr[i] : 0) + sdr[i] * (x % 3);
+    }, 0);
+}
 
 // Tests
-describe( "Greed is Good", function() {
-    it( "should value this as worthless", function() {
-      assert.strictEqual( score( [2, 3, 4, 6, 2] ), 0, "Incorrect answer for dice = [2, 3, 4, 6, 2]" );
+describe("Greed is Good", function () {
+    it("should value this as worthless", function () {
+        assert.strictEqual(score([2, 3, 4, 6, 2]), 0, "Incorrect answer for dice = [2, 3, 4, 6, 2]");
     });
-    
-    it( "should value this triplet correctly", function() {
-      assert.strictEqual( score( [4, 4, 4, 3, 3] ), 400, "Incorrect answer for dice = [4, 4, 4, 3, 3]" );
+
+    it("should value this triplet correctly", function () {
+        assert.strictEqual(score([4, 4, 4, 3, 3]), 400, "Incorrect answer for dice = [4, 4, 4, 3, 3]");
     });
-    
-    it( "should value this mixed set correctly", function() {
-      assert.strictEqual( score( [2, 4, 4, 5, 4] ), 450, "Incorrect answer for dice = [2, 4, 4, 5, 4]" );
+
+    it("should value this mixed set correctly", function () {
+        assert.strictEqual(score([2, 4, 4, 5, 4]), 450, "Incorrect answer for dice = [2, 4, 4, 5, 4]");
     });
-  });
+});
+
+/**
+ * PaginationHelper
+ *
+*/
+
+// Simple solution
+
+class PaginationHelper {
+    constructor(collection, itemsPerPage) {
+        this.collection = collection
+        this.itemsPerPage = itemsPerPage
+    }
+    itemCount() {
+        return this.collection.length
+    }
+    pageCount() {
+        return Math.ceil(this.collection.length / this.itemsPerPage) 
+    }
+    pageItemCount(pageIndex) {
+     
+        if (pageIndex === this.pageCount() - 1 && this.pageCount() > 0) {
+            return this.itemCount() - ((this.pageCount() - 1) * this.itemsPerPage)
+        } else if (pageIndex < this.pageCount() - 1 && pageIndex >= 0) {
+            return this.itemsPerPage
+        } else {
+            return -1
+        }
+    }
+    pageIndex(itemIndex) {
+        if (itemIndex > this.itemCount() - 1 || itemIndex < 0) {
+            return -1
+        } else {
+            return Math.floor(itemIndex / this.itemsPerPage)
+        }
+    }
+}
+
+
+
+// Tests
+describe("PaginationHelper", () => {
+
+
+    function doTest(instance, methodName, expected, ...args) {
+        const actual = instance[methodName](...args);
+        assert.strictEqual(actual, expected, `for ${methodName}(${args.join(', ')})`);
+    }
+
+    it("sample test : 24 items with 10 per page", () => {
+        const collection = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+        const helper = new PaginationHelper(collection, 10)
+
+        doTest(helper, 'pageCount', 3);
+        doTest(helper, 'itemCount', 24);
+
+        doTest(helper, 'pageItemCount', 10, 1);
+        doTest(helper, 'pageItemCount', 4, 2);
+        doTest(helper, 'pageItemCount', -1, 3);
+        doTest(helper, 'pageIndex', -1, 40);
+
+        doTest(helper, 'pageIndex', 2, 22);
+        doTest(helper, 'pageIndex', 0, 3);
+        doTest(helper, 'pageIndex', 0, 0);
+        doTest(helper, 'pageIndex', -1, -1);
+        doTest(helper, 'pageIndex', -1, -23);
+        doTest(helper, 'pageIndex', -1, -15);
+    });
+
+    it('empty collection', () => {
+        const empty = new PaginationHelper([], 10);
+
+        doTest(empty, 'pageCount', 0);
+        doTest(empty, 'itemCount', 0);
+        doTest(empty, 'pageIndex', -1, 0);
+        doTest(empty, 'pageItemCount', -1, 0);
+    });
+
+    function randInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    describe('random tests', () => {
+        for (let i = 0; i < 100; i++) {
+            const itemCount = randInt(0, 100);
+            const itemsPerPage = randInt(1, itemCount * 2);
+            const pagesCount = Math.ceil(itemCount / itemsPerPage);
+
+            const helper = new PaginationHelper(Array(itemCount), itemsPerPage);
+
+            it(`for itemCount = ${itemCount} itemsPerPage = ${itemsPerPage}`, function () {
+                doTest(helper, 'pageCount', pagesCount);
+                doTest(helper, 'itemCount', itemCount);
+
+                for (let i = 0; i < 5; i++) {
+                    const pageIndex = randInt(-2, pagesCount + 3);
+
+                    let pageItemCount;
+                    if (pageIndex < 0 || pageIndex >= pagesCount)
+                        pageItemCount = -1;
+                    else if (pageIndex === (pagesCount - 1)) // last page
+                        pageItemCount = (itemCount % itemsPerPage) || itemsPerPage;
+                    else
+                        pageItemCount = itemsPerPage;
+
+                    doTest(helper, 'pageItemCount', pageItemCount, pageIndex);
+                }
+
+                for (let i = 0; i < 5; i++) {
+                    const itemIndex = randInt(-2, itemCount + 5);
+                    const pageIndex = (itemIndex < 0 || itemIndex >= itemCount) ? -1 : Math.floor(itemIndex / itemsPerPage);
+
+                    doTest(helper, 'pageIndex', pageIndex, itemIndex);
+                }
+            });
+        }
+    });
+});

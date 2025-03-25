a1ac68ff517eb222477ce415784bc7b2cb7610ba
Artur <ghostmatt36@gmail.com>
2025-03-25 23:12:54 +0200

    Integers: Recreation One

--- index.js
+++ index.js
@@ -949,10 +949,10 @@ class PaginationHelper {
         return this.collection.length
     }
     pageCount() {
-        return Math.ceil(this.collection.length / this.itemsPerPage) 
+        return Math.ceil(this.collection.length / this.itemsPerPage)
     }
     pageItemCount(pageIndex) {
-     
+
         if (pageIndex === this.pageCount() - 1 && this.pageCount() > 0) {
             return this.itemCount() - ((this.pageCount() - 1) * this.itemsPerPage)
         } else if (pageIndex < this.pageCount() - 1 && pageIndex >= 0) {
@@ -1060,29 +1060,127 @@ describe("PaginationHelper", () => {
 // Simple solution
 
 
-function productFib(prod){
+function productFib(prod) {
     let a = 1
     let b = 1
-    while((a*b) < prod){
-     let next = a+b
-     a = b
-     b = next
- 
+    while ((a * b) < prod) {
+        let next = a + b
+        a = b
+        b = next
+
     }
-    return [a, b, a*b === prod]
- 
- }
+    return [a, b, a * b === prod]
+
+}
 
 // Tests
 describe("Product of consecutive Fib numbers", () => {
     it("Product of consecutive Fib numbers", () => {
-      assert.sameOrderedMembers(productFib(4895), [55, 89, true])
-      assert.sameOrderedMembers(productFib(5895), [89, 144, false])
-      assert.sameOrderedMembers(productFib(74049690), [6765, 10946, true])
-      assert.sameOrderedMembers(productFib(84049690), [10946, 17711, false])
-      assert.sameOrderedMembers(productFib(193864606), [10946, 17711, true])
-      assert.sameOrderedMembers(productFib(447577), [610, 987, false])
-      assert.sameOrderedMembers(productFib(602070), [610, 987, true])
+        assert.sameOrderedMembers(productFib(4895), [55, 89, true])
+        assert.sameOrderedMembers(productFib(5895), [89, 144, false])
+        assert.sameOrderedMembers(productFib(74049690), [6765, 10946, true])
+        assert.sameOrderedMembers(productFib(84049690), [10946, 17711, false])
+        assert.sameOrderedMembers(productFib(193864606), [10946, 17711, true])
+        assert.sameOrderedMembers(productFib(447577), [610, 987, false])
+        assert.sameOrderedMembers(productFib(602070), [610, 987, true])
     });
-  });
-  
+});
+
+/**
+ * Integers: Recreation One
+ * 1, 246, 2, 123, 3, 82, 6, 41 are the divisors of number 246.
+ * Squaring these divisors we get: 1, 60516, 4, 15129, 9, 6724, 36, 1681.
+ * The sum of these squares is 84100 which is 290 * 290.
+ *
+*/
+
+// Simple solution
+function listSquared(m, n) {
+    let start = m
+    let finish = n
+    const result = []
+
+    function divisor(n) {
+        const div = new Set()
+        for (let i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i === 0) {
+                div.add(i)
+                div.add(n / i);
+            }
+
+        }
+        return [...div]
+
+    }
+    for (let i = start; i <= finish; i++) {
+        const arr = divisor(i)
+        const sum = arr.map(e =>
+            Math.pow(e, 2)
+        ).reduce((acc, e) => acc + e, 0)
+        sum
+        if (Number.isInteger(Math.sqrt(sum))) {
+            result.push([i, sum])
+        }
+
+
+    }
+    return result
+}
+
+// Tests
+describe('Integers: Recreation One...', function () {
+    it("Basic tests", function () {
+        assert.deepEqual(listSquared(1, 250), [[1, 1], [42, 2500], [246, 84100]])
+        assert.deepEqual(listSquared(42, 250), [[42, 2500], [246, 84100]])
+        assert.deepEqual(listSquared(250, 500), [[287, 84100]])
+        assert.deepEqual(listSquared(300, 600), [])
+        assert.deepEqual(listSquared(600, 1500), [[728, 722500], [1434, 2856100]])
+        assert.deepEqual(listSquared(1500, 1800), [[1673, 2856100]])
+        assert.deepEqual(listSquared(1800, 2000), [[1880, 4884100]])
+        assert.deepEqual(listSquared(2000, 2200), [])
+        assert.deepEqual(listSquared(2200, 5000), [[4264, 24304900]])
+        assert.deepEqual(listSquared(5000, 10000), [[6237, 45024100], [9799, 96079204], [9855, 113635600]])
+    })
+
+    it("Random tests", function () {
+        function randint(a, b) {
+            return Math.floor(Math.random() * (b - a + 1) + a);
+        }
+
+        function solAux130412(n) {
+            let s = 0;
+            let nf = 0;
+            let res = [];
+            for (let i = 1; i <= Math.floor(Math.sqrt(n)); i += 1)
+                if (n % i === 0) {
+                    s += i * i;
+                    nf = n / i;
+                    if (nf !== i)
+                        s += nf * nf;
+                }
+            if (Math.pow(~~Math.sqrt(s), 2) === s) {
+                res.push(n);
+                res.push(s);
+                return res;
+            } else return null;
+        }
+
+        function solution130412(m, n) {
+            let res = [];
+            for (let i = m; i <= n; i++) {
+                let r = solAux130412(i);
+                if (r !== null) {
+                    res.push(r);
+                }
+            }
+            return res;
+        }
+
+        for (let i = 0; i < 50; i++) {
+            let m = randint(200, 1000);
+            let n = randint(1100, 8000);
+            let exp = solution130412(m, n)
+            assert.deepEqual(listSquared(m, n), exp)
+        }
+    })
+})

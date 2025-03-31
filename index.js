c71d4e09dcca875f161e2223f9f00006926bba9e
Artur <ghostmatt36@gmail.com>
2025-03-31 23:39:56 +0300

    Tic-Tac-Toe Checker

--- index.js
+++ index.js
@@ -1426,3 +1426,49 @@ describe("groupPalindromes", function () {
         );
     });
 });
+
+/**
+ * Tic-Tac-Toe Checker
+ * Assume that the board comes in the form of a 3x3 array, where the value is 0 if a spot is empty, 1 if it is an "X", or 2 if it is an "O", like so:
+*/
+
+// Simple solution
+function isSolved(board) {
+    for (let i = 0; i < 3; i++) {
+
+        if (board[i][0] && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+            return board[i][0];
+        }
+        if (board[0][i] && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+            return board[0][i];
+        }
+    }
+    
+
+    if (board[0][0] && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+        return board[0][0];
+    }
+    if (board[0][2] && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+        return board[0][2];
+    }
+    
+  
+    for (let row of board) {
+        if (row.includes(0)) {
+            return -1;
+        }
+    }
+    
+    return 0;
+}
+
+
+
+// Tests
+describe("Tic-Tac-Toe Checker", () => {
+    it("test", () => {
+      assert.strictEqual(isSolved([[0,0,1],
+                                   [0,1,2],
+                                   [2,1,0]]), -1);
+    });
+  });

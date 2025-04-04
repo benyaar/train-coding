6597a32193a379972a192bcd43a4f83fe1fea07f
Artur <82472015+benyaar@users.noreply.github.com>
2025-04-04 09:42:53 +0300

    countBits

--- index.js
+++ index.js
@@ -1601,3 +1601,62 @@ describe('digitalRoot', function () {
         }
     })
 })
+
+/**
+ * countBits,
+ * Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+*/
+
+// Simple solution
+
+function countBits (n) {
+    return n.toString(2).split('').reduce((a, b) => Number(a) + Number(b), 0)
+};
+
+// Best solution
+function countBits2 (n) {
+    return n.toString(2).split('0').join('').length;
+};
+
+// Tests
+describe("countBits", () => {
+    it("Testing for fixed tests", () => {
+      assert.strictEqual(countBits(0), 0);
+      assert.strictEqual(countBits(4), 1);
+      assert.strictEqual(countBits(7), 3);
+      assert.strictEqual(countBits(9), 2);
+      assert.strictEqual(countBits(10), 2);
+      assert.strictEqual(countBits(26), 3);
+      assert.strictEqual(countBits(77231418), 14);
+      assert.strictEqual(countBits(12525589), 11);
+      assert.strictEqual(countBits(3811), 8);
+      assert.strictEqual(countBits(392902058), 17);
+      assert.strictEqual(countBits(1044), 3);
+      assert.strictEqual(countBits(10030245), 10);
+      assert.strictEqual(countBits(183337941), 16);
+      assert.strictEqual(countBits(20478766), 14);
+      assert.strictEqual(countBits(103021), 9);
+      assert.strictEqual(countBits(287), 6);
+      assert.strictEqual(countBits(115370965), 15);
+      assert.strictEqual(countBits(31), 5);
+      assert.strictEqual(countBits(417862), 7);
+      assert.strictEqual(countBits(626031), 12);
+      assert.strictEqual(countBits(89), 4);
+      assert.strictEqual(countBits(674259), 10);
+      })
+    })
+  
+  describe("Random tests", () => {  
+      
+    function randint(min, max){
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+    for(let i = 0; i < 100; i++){
+      let num = randint(0, 10**randint(1, 10))
+      let expected = [...num.toString(2)].reduce((a, b) => a + +b, 0)
+      it(`countBits(${num}) should equal ${expected}`, () => {
+        assert.strictEqual(countBits(num), expected);
+      });
+    }
+  })

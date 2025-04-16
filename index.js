1013106c6ef79d4dd84cde7b55b3f95cfdb44e2e
Artur <ghostmatt36@gmail.com>
2025-04-16 23:31:32 +0300

    Your order, please

--- index.js
+++ index.js
@@ -2052,3 +2052,35 @@ describe("Persistent Bugger.", () => {
       assert.strictEqual(persistence(999),4);
     });
   });
+
+  /**
+   * Your order, please
+   *
+  */
+  
+  // Simple solution
+  
+  function order(words) {
+    if (!words) return "";
+  
+    return words
+      .split(" ")
+      .sort((a, b) => {
+        const numA = a.match(/\d/)[0];
+        const numB = b.match(/\d/)[0];
+        return numA - numB;
+      })
+      .join(" ");
+  }
+  
+  // Best solution
+  
+  
+  // Tests
+  describe("Your order, please", () => {
+    it("should work corectly", () => {
+      assert.strictEqual(order("is2 Thi1s T4est 3a"), "Thi1s is2 3a T4est")
+      assert.strictEqual(order("4of Fo1r pe6ople g3ood th5e the2"), "Fo1r the2 g3ood 4of th5e pe6ople")
+      assert.strictEqual(order(""), "", "empty input should return empty string" )
+    });
+  });

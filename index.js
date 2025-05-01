061d7f6893ca4d2f27116deece32d7ad5cac5f0f
Artur <ghostmatt36@gmail.com>
2025-05-01 23:04:38 +0300

    Build a pile of Cubes

--- index.js
+++ index.js
@@ -2473,3 +2473,30 @@ describe("lookAndSay", function () {
         assert.deepEqual(lookAndSay(data, len), expected, `Failed for input: "${data}"\n`);
     }
 });
+
+/**
+ * Build a pile of Cubes
+ *
+*/
+
+// Simple solution
+function findNb(m) {
+    let n = 0;
+    let total = 0;
+  
+    while (total < m) {
+      n++;
+      total += Math.pow(n, 3);
+    }
+  
+    return total === m ? n : -1;
+  }
+
+// Tests
+it("Build a pile of Cubes",function() {
+    assert.strictEqual(findNb(4183059834009), 2022)
+    assert.strictEqual(findNb(24723578342962), -1)
+    assert.strictEqual(findNb(135440716410000), 4824)
+    assert.strictEqual(findNb(40539911473216), 3568)
+  })
+  

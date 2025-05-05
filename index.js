9330d539947d6e32e1dba764a5affca7ef7f0b36
Artur <ghostmatt36@gmail.com>
2025-05-05 23:44:34 +0300

    Pick peaks

--- index.js
+++ index.js
@@ -2591,3 +2591,44 @@ describe("Josephus Survivor", function () {
         assert.strictEqual(josephusSurvivor(100, 1), 100);
     });
 });
+
+/**
+ * Pick peaks
+*/
+
+// Simple solution
+function pickPeaks(arr) {
+    const result = { pos: [], peaks: [] };
+    let possiblePeakPos = null;
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > arr[i - 1]) {
+            possiblePeakPos = i;
+        } else if (arr[i] < arr[i - 1] && possiblePeakPos !== null) {
+
+            result.pos.push(possiblePeakPos);
+            result.peaks.push(arr[possiblePeakPos]);
+            possiblePeakPos = null;
+        }
+    }
+
+    return result;
+}
+
+
+// Tests
+
+describe("Pick peaks", function () {
+    it("Tests", function () {
+        assert.deepEqual(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1]), { pos: [3, 7], peaks: [6, 3] });
+        assert.deepEqual(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]), { pos: [3, 7], peaks: [6, 3] });
+        assert.deepEqual(pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 2, 2, 1]), { pos: [3, 7, 10], peaks: [6, 3, 2] });
+        assert.deepEqual(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2, 1]), { pos: [2, 4], peaks: [3, 2] });
+        assert.deepEqual(pickPeaks([2, 1, 3, 1, 2, 2, 2, 2]), { pos: [2], peaks: [3] });
+        assert.deepEqual(pickPeaks([2, 1, 3, 2, 2, 2, 2, 5, 6]), { pos: [2], peaks: [3] });
+        assert.deepEqual(pickPeaks([2, 1, 3, 2, 2, 2, 2, 1]), { pos: [2], peaks: [3] });
+        assert.deepEqual(pickPeaks([1, 2, 5, 4, 3, 2, 3, 6, 4, 1, 2, 3, 3, 4, 5, 3, 2, 1, 2, 3, 5, 5, 4, 3]), { pos: [2, 7, 14, 20], peaks: [5, 6, 5, 5] });
+        assert.deepEqual(pickPeaks([]), { pos: [], peaks: [] });
+        assert.deepEqual(pickPeaks([1, 1, 1, 1]), { pos: [], peaks: [] });
+    });
+});

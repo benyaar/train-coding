e95206c4ccda97c953b781f832b9442802ee327a
Artur <82472015+benyaar@users.noreply.github.com>
2025-07-23 23:49:31 +0300

    Array Helpers

--- index.js
+++ index.js
@@ -2775,3 +2775,48 @@ describe( "diamond()", function(){
     assert.strictEqual(diamond(0), null)
   });
 });
+
+/**
+ * Array Helpers
+ *
+*/
+
+// Simple solution
+Array.prototype.square = function () {
+  return this.map(n => n ** 2);
+};
+
+Array.prototype.cube = function () {
+  return this.map(n => n ** 3);
+};
+
+Array.prototype.average = function () {
+  return this.length === 0 ? NaN : this.sum() / this.length;
+};
+
+Array.prototype.sum = function () {
+  return this.reduce((acc, val) => acc + val, 0);
+};
+
+Array.prototype.even = function () {
+  return this.filter(n => n % 2 === 0);
+};
+
+Array.prototype.odd = function () {
+  return this.filter(n => n % 2 !== 0);
+};
+
+
+// Tests
+describe("Array Helpers", () => {
+  it("Array Helpers", () => {
+    const numbers = [1, 2, 3, 4, 5];
+    assert.deepEqual(numbers.square(), [1, 4, 9, 16, 25]);
+    assert.deepEqual(numbers.cube(), [1, 8, 27, 64, 125]);
+    assert.strictEqual(numbers.sum(), 15, 'Wrong sum');
+    assert.strictEqual(numbers.average(), 3, 'Wrong average');
+    assert.deepEqual(numbers.even(), [2, 4], 'Wrong result for even()');
+    assert.deepEqual(numbers.odd(), [1, 3, 5], 'Wrong result for odd()');
+  });
+});
+

abb3e1c45295c733aed9135beead86939dd002e5
Artur <82472015+benyaar@users.noreply.github.com>
2025-07-24 23:10:08 +0300

    Stop gninnipS My sdroW!

--- index.js
+++ index.js
@@ -2820,3 +2820,29 @@ describe("Array Helpers", () => {
   });
 });
 
+/**
+ * Stop gninnipS My sdroW!
+ *
+*/
+
+// Simple solution
+
+function spinWords(sentence) {
+  return sentence
+    .split(' ')
+    .map(word => word.length >= 5 ? word.split('').reverse().join('') : word)
+    .join(' ');
+}
+
+// Tests
+describe("Stop gninnipS My sdroW!",()=>{
+  it("Stop gninnipS My sdroW!",()=>{
+    assert.strictEqual(spinWords("Welcome"), "emocleW");
+    assert.strictEqual(spinWords("Hey fellow warriors"), "Hey wollef sroirraw");
+    assert.strictEqual(spinWords("This is a test"), "This is a test");
+    assert.strictEqual(spinWords("This is another test"), "This is rehtona test");
+    assert.strictEqual(spinWords("You are almost to the last test"), "You are tsomla to the last test");
+    assert.strictEqual(spinWords("Just kidding there is still one more"), "Just gniddik ereht is llits one more");
+    assert.strictEqual(spinWords("Seriously this is the last one"), "ylsuoireS this is the last one");
+  });
+});

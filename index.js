0be02860b96533ff27e5ad3e362e96cd25809099
Artur <ghostmatt36@gmail.com>
2025-09-23 08:48:03 +0300

    Two Sum

--- index.js
+++ index.js
@@ -2936,3 +2936,50 @@ describe('WeIrD StRiNg CaSe', function() {
         doTest('lower case', 'LoWeR CaSe');
     });
 });
+
+/**
+ * Two Sum
+ *
+*/
+
+// Simple solution
+function twoSum(numbers, target) {
+  const seen = new Map(); 
+
+  for (let i = 0; i < numbers.length; i++) {
+    const complement = target - numbers[i];
+
+    if (seen.has(complement)) {
+      return [seen.get(complement), i];
+    }
+
+    seen.set(numbers[i], i);
+  }
+  
+  return null; 
+}
+// Tests
+describe("Two Sum", function () {
+
+  function doTest(numbers, targetSum) {
+    const [idx0, idx1] = twoSum(numbers.slice(), targetSum);
+    const num0 = numbers[idx0], num1 = numbers[idx1], actSum = num0 + num1;
+
+    const message = `for target sum = ${targetSum} and numbers = [${numbers.join(', ')}]\n`;
+
+    assert.notEqual(idx0, idx1,
+      message + `the two indices must be different, but got [${[idx0, idx1]}]\n`
+    );
+    assert.strictEqual(actSum, targetSum,
+      message + `array[${idx0}] = ${num0}, array[${idx1}] = ${num1}\n` +
+      `${num0} + ${num1} = ${actSum}\n`
+    );
+  }
+
+  it('Sample Tests', function () {
+    doTest([1, 2, 3], 4);
+    doTest([1234, 5678, 9012], 14690);
+    doTest([2, 2, 3], 4);
+    doTest([2, 3, 1], 3);
+  })
+});

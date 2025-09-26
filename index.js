8df7c36da628b6e4fc9ad1a18f4ae4defadfcb12
Artur <82472015+benyaar@users.noreply.github.com>
2025-09-26 23:47:47 +0300

    Sort the odd

--- index.js
+++ index.js
@@ -3057,3 +3057,26 @@ describe("Title Case", () => {
     assert.equal(titleCase('the quick brown fox'), 'The Quick Brown Fox')
   });
 });
+
+/**
+ * Sort the odd
+ *
+*/
+
+// Simple solution
+
+function sortArray(array) {
+
+  const odds = array.filter(x => x % 2 !== 0).sort((a, b) => a - b);
+  
+  return array.map(x => x % 2 !== 0 ? odds.shift() : x);
+}
+
+// Tests
+describe("Sort the odd", () => {
+  it("Sort the odd", () => {
+    assert.deepEqual(sortArray([5, 3, 2, 8, 1, 4]), [1, 3, 2, 8, 5, 4]);
+    assert.deepEqual(sortArray([5, 3, 1, 8, 0]), [1, 3, 5, 8, 0]);
+    assert.deepEqual(sortArray([]),[]);
+  });
+});
